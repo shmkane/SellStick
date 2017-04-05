4cf16bc56d788de24f32484c45d646c81ed9d339
shmkane <shmkane@gmail.com>
2017-04-05 10:44:02 -0400

    Added (main) SellStick Class

---
+++ com/acropolismc/play/sellstick/SellStick.java
@@ -0,0 +1,45 @@
+package com.acropolismc.play.sellstick;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.economy.Economy;
+
+public class SellStick extends JavaPlugin{
+
+	private static Economy econ = null;
+    private static final Logger log = Logger.getLogger("Minecraft");
+
+    
+	public void onEnable() {
+
+		if (!setupEconomy() ) {
+			log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+	}
+	
+	public Economy getEcon() {
+		return SellStick.econ;
+	}
+	
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+	public void handleConfig(){
+
+	}
+
+}

31b1cdc25c568b1f1ebb7b9936ffc204c7016b8c
shmkane <spaikan@ncsu.edu>
2019-08-19 20:55:09 -0400

    Created helper metof

--- src/com/shmkane/sellstick/PlayerListener.java
+++ src/com/shmkane/sellstick/PlayerListener.java
@@ -160,6 +160,29 @@ public class PlayerListener implements Listener {
 		return found;
 	}
 
+	/**
+	 * Determines if a player clicked a chest using a sellstick or not.
+	 * @param p
+	 * @param e
+	 * @return
+	 */
+	public boolean didClickChestWithSellStick(Player p, PlayerInteractEvent e) {
+		Material sellItem = Material.getMaterial(StickConfig.instance.item.toUpperCase());
+
+		if (p.getItemInHand().getType() == sellItem) {
+			if (p.getItemInHand().getItemMeta().getDisplayName() != null
+					&& p.getItemInHand().getItemMeta().getDisplayName().startsWith(StickConfig.instance.name)) {
+				if (e.getClickedBlock().getType() == Material.CHEST
+						|| e.getClickedBlock().getType() == Material.TRAPPED_CHEST) {
+
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
 	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.HIGHEST)
 
@@ -178,186 +201,173 @@ public class PlayerListener implements Listener {
 		// When they left click with that item, and that item has the same name
 		// as a sellstick
 		if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if (didClickChestWithSellStick(p, e)) {
 
-			// Leaving this deprecated method for some backwards compatibility. Anything
-			// after
-			// 1.9 should have p.getInventory().getItemInMainHand(), or offhand, or
-			// whatever.
-			if (p.getItemInHand().getType() == sellItem) {
-				if (p.getItemInHand().getItemMeta().getDisplayName() != null
-						&& p.getItemInHand().getItemMeta().getDisplayName().startsWith(StickConfig.instance.name)) {
-					if (e.getClickedBlock().getType() == Material.CHEST
-							|| e.getClickedBlock().getType() == Material.TRAPPED_CHEST) {
-
-						// Other plugin overriden.
-						if (e.isCancelled()) {
-							plugin.msg(p, StickConfig.instance.territoryMessage);
-							e.setCancelled(true);
-							return;
-						}
-
-						// Didn't have permission :(
-						if (!p.hasPermission("sellstick.use")) {
-							plugin.msg(p, StickConfig.instance.noPerm);
-							e.setCancelled(true);
-							return;
-						}
-
-						ItemStack is = p.getItemInHand();
-						ItemMeta im = is.getItemMeta();
-
-						// So here we go
-						List<String> lores = im.getLore();
-						int uses = -1;
-						if (!isInfinite(lores)) {
-							uses = getUsesFromLore(lores);
-						}
-						if (uses == -2) {
-							// This should honestly never happen unless someone changes sellstick lores
-							plugin.msg(p, ChatColor.RED + "There was an error!");
-							plugin.msg(p, ChatColor.RED
-									+ "Please let an admin know to check console, or, send them these messages:");
-
-							plugin.msg(p, ChatColor.RED
-									+ "Player has a sellstick that has had its 'DurabilityLine' changed in the config");
-							plugin.msg(p, ChatColor.RED
-									+ "For this reason, the plugin could not find the line number on which the finite/infinite lore exists");
-							plugin.msg(p, ChatColor.RED + "This can be resolved by either:");
-							plugin.msg(p, ChatColor.RED + "1: Giving the player a new sellstick");
-							plugin.msg(p, ChatColor.RED + "(Includes anyone on the server that has this issue)");
-							plugin.msg(p, ChatColor.RED + "or");
-							plugin.msg(p, ChatColor.RED
-									+ "2: Changing the DurabilityLine to match the one that is on this sellstick");
-
-							plugin.msg(p, ChatColor.RED + "For help, contact shmkane on spigot or github");
-
-							return;
-						}
-
-						InventoryHolder c = (InventoryHolder) e.getClickedBlock().getState();
-						ItemStack[] contents = (ItemStack[]) c.getInventory().getContents();
-						double total = 0;
-						double slotPrice = 0;
-						double price = 0;
-						double multiplier = Double.NEGATIVE_INFINITY;
-
-						for (int i = 0; i < c.getInventory().getSize(); i++) {
-							try {
-								if (!StickConfig.instance.useEssentialsWorth || plugin.ess == null
-										|| !plugin.ess.isEnabled()) {
-									for (String key : PriceConfig.instance.getConfig().getConfigurationSection("prices")
-											.getKeys(false)) {
-
-										int data;
-										String name;
-
-										if (!key.contains(":")) {
-											data = 0;
-											name = key;
-										} else {
-											name = (key.split(":"))[0];
-											data = Integer.parseInt(key.split(":")[1]);
-										}
-
-										if ((contents[i].getType().toString().equalsIgnoreCase(name) || (isNumeric(name)
-												&& contents[i].getType().getId() == Integer.parseInt(name)))
-												&& contents[i].getDurability() == data) {
-											price = Double.parseDouble(
-													PriceConfig.instance.getConfig().getString("prices." + key));
-
-										}
-									}
-								} else {
-									// Essentials Worth
-									price = plugin.ess.getWorth().getPrice(plugin.ess, contents[i]).doubleValue();
-								}
-
-								int amount = (int) contents[i].getAmount();
-
-								slotPrice = price * amount;
-								if (slotPrice > 0) {
-									ItemStack sell = contents[i];
-									c.getInventory().remove(sell);
-									e.getClickedBlock().getState().update();
-								}
-
-							} catch (NullPointerException ex) {
-							}
-
-							total += slotPrice;
-							slotPrice = 0;
-							price = 0;
-						}
-
-						if (total > 0) {
-							if (!isInfinite(lores)) {
-
-								lores.set(StickConfig.instance.durabilityLine - 1,
-										lores.get(StickConfig.instance.durabilityLine - 1).replaceAll(uses + "",
-												(uses - 1) + ""));
-								im.setLore(lores);
-								is.setItemMeta(im);
-							}
-
-							/*
-							 * Permissions based multiplier check. If user doesn't have
-							 * sellstick.multiplier.x permission Multiplier defaults to 1 as seen below.
-							 */
-							for (PermissionAttachmentInfo perm : p.getEffectivePermissions()) {
-								if (perm.getPermission().startsWith("sellstick.multiplier")) {
-									String stringPerm = perm.getPermission();
-									String permSection = stringPerm.replaceAll("sellstick.multiplier.", "");
-									if (Double.parseDouble(permSection) > multiplier) {
-										multiplier = Double.parseDouble(permSection);
-									}
-								}
-							}
-
-							/*
-							 * Multiplier set to Double.NEGATIVE_INFINITY by default to signal "unchanged"
-							 * Problem with defaulting to 0 is total*0 = 0, Problem with defaulting to 1 is
-							 * multipliers < 1.
-							 */
-							EconomyResponse r;
-
-							if (multiplier == Double.NEGATIVE_INFINITY) {
-								r = plugin.getEcon().depositPlayer(p, total);
-							} else {
-								r = plugin.getEcon().depositPlayer(p, total * multiplier);
-							}
-
-							if (r.transactionSuccess()) {
-								if (StickConfig.instance.sellMessage.contains("\\n")) {
-									String[] send = StickConfig.instance.sellMessage.split("\\\\n");
-									for (String msg : send) {
-										plugin.msg(p, msg.replace("%balance%", plugin.getEcon().format(r.balance))
-												.replace("%price%", plugin.getEcon().format(r.amount)));
-									}
-								} else {
-									plugin.msg(p,
-											StickConfig.instance.sellMessage
-													.replace("%balance%", plugin.getEcon().format(r.balance))
-													.replace("%price%", plugin.getEcon().format(r.amount)));
-								}
-
-								System.out.println(p.getName() + " sold items via sellstick for " + r.amount
-										+ " and now has " + r.balance);
-							} else {
-								plugin.msg(p, String.format("An error occured: %s", r.errorMessage));
-							}
-
-							if (uses - 1 == 0) {
-								p.getInventory().remove(p.getItemInHand());
-								p.updateInventory();
-								plugin.msg(p, StickConfig.instance.brokenStick);
-							}
-
-						} else {
-							plugin.msg(p, StickConfig.instance.nothingWorth);
-						}
-						e.setCancelled(true);
-					}
+				// Other plugin overriden.
+				if (e.isCancelled()) {
+					plugin.msg(p, StickConfig.instance.territoryMessage);
+					e.setCancelled(true);
+					return;
 				}
+
+				// Didn't have permission :(
+				if (!p.hasPermission("sellstick.use")) {
+					plugin.msg(p, StickConfig.instance.noPerm);
+					e.setCancelled(true);
+					return;
+				}
+
+				ItemStack is = p.getItemInHand();
+				ItemMeta im = is.getItemMeta();
+
+				// So here we go
+				List<String> lores = im.getLore();
+				int uses = -1;
+				if (!isInfinite(lores)) {
+					uses = getUsesFromLore(lores);
+				}
+				if (uses == -2) {
+					// This should honestly never happen unless someone changes sellstick lores
+					plugin.msg(p, ChatColor.RED + "There was an error!");
+					plugin.msg(p,
+							ChatColor.RED + "Please let an admin know to check console, or, send them these messages:");
+
+					plugin.msg(p, ChatColor.RED
+							+ "Player has a sellstick that has had its 'DurabilityLine' changed in the config");
+					plugin.msg(p, ChatColor.RED
+							+ "For this reason, the plugin could not find the line number on which the finite/infinite lore exists");
+					plugin.msg(p, ChatColor.RED + "This can be resolved by either:");
+					plugin.msg(p, ChatColor.RED + "1: Giving the player a new sellstick");
+					plugin.msg(p, ChatColor.RED + "(Includes anyone on the server that has this issue)");
+					plugin.msg(p, ChatColor.RED + "or");
+					plugin.msg(p, ChatColor.RED
+							+ "2: Changing the DurabilityLine to match the one that is on this sellstick");
+
+					plugin.msg(p, ChatColor.RED + "For help, contact shmkane on spigot or github");
+
+					return;
+				}
+
+				InventoryHolder c = (InventoryHolder) e.getClickedBlock().getState();
+				ItemStack[] contents = (ItemStack[]) c.getInventory().getContents();
+				double total = 0;
+				double slotPrice = 0;
+				double price = 0;
+				double multiplier = Double.NEGATIVE_INFINITY;
+
+				for (int i = 0; i < c.getInventory().getSize(); i++) {
+					try {
+						if (!StickConfig.instance.useEssentialsWorth || plugin.ess == null || !plugin.ess.isEnabled()) {
+							for (String key : PriceConfig.instance.getConfig().getConfigurationSection("prices")
+									.getKeys(false)) {
+
+								int data;
+								String name;
+
+								if (!key.contains(":")) {
+									data = 0;
+									name = key;
+								} else {
+									name = (key.split(":"))[0];
+									data = Integer.parseInt(key.split(":")[1]);
+								}
+
+								if ((contents[i].getType().toString().equalsIgnoreCase(name)
+										|| (isNumeric(name) && contents[i].getType().getId() == Integer.parseInt(name)))
+										&& contents[i].getDurability() == data) {
+									price = Double
+											.parseDouble(PriceConfig.instance.getConfig().getString("prices." + key));
+
+								}
+							}
+						} else {
+							// Essentials Worth
+							price = plugin.ess.getWorth().getPrice(plugin.ess, contents[i]).doubleValue();
+						}
+
+						int amount = (int) contents[i].getAmount();
+
+						slotPrice = price * amount;
+						if (slotPrice > 0) {
+							ItemStack sell = contents[i];
+							c.getInventory().remove(sell);
+							e.getClickedBlock().getState().update();
+						}
+
+					} catch (NullPointerException ex) {
+					}
+
+					total += slotPrice;
+					slotPrice = 0;
+					price = 0;
+				}
+
+				if (total > 0) {
+					if (!isInfinite(lores)) {
+
+						lores.set(StickConfig.instance.durabilityLine - 1, lores
+								.get(StickConfig.instance.durabilityLine - 1).replaceAll(uses + "", (uses - 1) + ""));
+						im.setLore(lores);
+						is.setItemMeta(im);
+					}
+
+					/*
+					 * Permissions based multiplier check. If user doesn't have
+					 * sellstick.multiplier.x permission Multiplier defaults to 1 as seen below.
+					 */
+					for (PermissionAttachmentInfo perm : p.getEffectivePermissions()) {
+						if (perm.getPermission().startsWith("sellstick.multiplier")) {
+							String stringPerm = perm.getPermission();
+							String permSection = stringPerm.replaceAll("sellstick.multiplier.", "");
+							if (Double.parseDouble(permSection) > multiplier) {
+								multiplier = Double.parseDouble(permSection);
+							}
+						}
+					}
+
+					/*
+					 * Multiplier set to Double.NEGATIVE_INFINITY by default to signal "unchanged"
+					 * Problem with defaulting to 0 is total*0 = 0, Problem with defaulting to 1 is
+					 * multipliers < 1.
+					 */
+					EconomyResponse r;
+
+					if (multiplier == Double.NEGATIVE_INFINITY) {
+						r = plugin.getEcon().depositPlayer(p, total);
+					} else {
+						r = plugin.getEcon().depositPlayer(p, total * multiplier);
+					}
+
+					if (r.transactionSuccess()) {
+						if (StickConfig.instance.sellMessage.contains("\\n")) {
+							String[] send = StickConfig.instance.sellMessage.split("\\\\n");
+							for (String msg : send) {
+								plugin.msg(p, msg.replace("%balance%", plugin.getEcon().format(r.balance))
+										.replace("%price%", plugin.getEcon().format(r.amount)));
+							}
+						} else {
+							plugin.msg(p,
+									StickConfig.instance.sellMessage
+											.replace("%balance%", plugin.getEcon().format(r.balance))
+											.replace("%price%", plugin.getEcon().format(r.amount)));
+						}
+
+						System.out.println(p.getName() + " sold items via sellstick for " + r.amount + " and now has "
+								+ r.balance);
+					} else {
+						plugin.msg(p, String.format("An error occured: %s", r.errorMessage));
+					}
+
+					if (uses - 1 == 0) {
+						p.getInventory().remove(p.getItemInHand());
+						p.updateInventory();
+						plugin.msg(p, StickConfig.instance.brokenStick);
+					}
+
+				} else {
+					plugin.msg(p, StickConfig.instance.nothingWorth);
+				}
+				e.setCancelled(true);
 			}
 		}
 	}
